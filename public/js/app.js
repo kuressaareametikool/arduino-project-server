6a132c9fcefb38cf340b603b3e3e837e079ea2c7
ooker <maaker+github@gmail.com>
2021-12-12 17:17:06 +0200

    separating server and client

--- public/js/app.js
+++ public/js/app.js
@@ -3,8 +3,6 @@ const socket = io();
 socket.on('connection', userId => {
   console.log("Connection established");
 });
-socket.on('send note', msgObj => {
-  console.log(msgObj.note);
-  //playSample(s2);  // play sample file if sample player is used
-  playSynth();    // play synth 
+socket.on('send-data', dataObj => {
+  console.log(JSON.stringify(dataObj));
 });
